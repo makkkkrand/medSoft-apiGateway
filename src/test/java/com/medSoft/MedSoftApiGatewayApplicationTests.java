b768a7cae789f8bc493f751c3ed75882fa5c7aeb
mukundkumarjha <mukundkumarjha@localhost>
2021-04-11 18:17:53 +0530

    initial commit

---
+++ src/test/java/com/medSoft/MedSoftApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedSoftApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
